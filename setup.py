bd69ba19ea82970fc094f8569238b8e4e34da63a
Beyang Liu <beyang@sourcegraph.com>
2016-07-03 18:19:26 -0700

    python2 syntax files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+print "python-2-style print statement in setup.py"
+
+setup(
+    name='python27-syntax',
+    version='0.0.0',
+    url='http://github.com/sgtest/python27-syntax',
+    py_modules=['py27lib'],
+    install_requires=[],
+)
